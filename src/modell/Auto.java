cbc71246b500bc9b7410591d7daaa415f3f4b0fe
WeinbergerPeti <weinbergerpeti@gmail.com>
2023-02-13 14:51:20 +0100

    Metódusok nem működnek

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -4,20 +4,24 @@ public class Auto extends Jarmu
 {
     private boolean defekt;
 
-    public Auto(boolean defekt) 
+    public Auto(boolean defekt, boolean beinditva, boolean uzemanyag, boolean megerkezett) 
     {
+        super(beinditva, uzemanyag, megerkezett);
         this.defekt = defekt;
     }
 
-    public boolean isDefekt() {
+    public boolean isDefekt() 
+    {
         return defekt;
     }
     
-    
-    
     public void kereketCserel()
     {
-        
+        String szoveg="";
+        if(isDefekt()==true)
+        {
+            szoveg="Defekt! Kereket kell cserélni!";
+        }
     }
     
     public boolean halad()
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -1,23 +1,55 @@
 package modell;
 
-public class Jarmu 
+public abstract class Jarmu 
 {
     private boolean beinditva;
     private boolean uzemanyag;
     private boolean megerkezett;
+
+    public Jarmu(boolean beinditva, boolean uzemanyag, boolean megerkezett) 
+    {
+        this.beinditva = beinditva;
+        this.uzemanyag = uzemanyag;
+        this.megerkezett = megerkezett;
+    }
+
+    public boolean isBeinditva() {
+        return beinditva;
+    }
+
+    public boolean isUzemanyag() {
+        return uzemanyag;
+    }
+
+    public boolean isMegerkezett() {
+        return megerkezett;
+    }
     
     public void beindit()
     {
-
+        String szoveg="";
+        if(isBeinditva()==true)
+        {
+            szoveg="Beindítva!";
+        }
     }
     
     public void leallit()
     {
-        
+        String szoveg="";
+        if(isMegerkezett()==true)
+        {
+            szoveg="Megérkezett a motor leáll!";
+        }
+        else if(tankol()==true)
+        {
+            szoveg="Tankol a motor leáll!";
+        }
     }
     
     public boolean tankol()
     {
+        
         return false;
     }
     
